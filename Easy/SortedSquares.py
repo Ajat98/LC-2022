82e13bab5a7a960723fc5775a2e263a18114a1fd
Adam Harner <adamj.harner@gmail.com>
2022-06-14 09:44:33 -0400

    Create SortedSquares.py

---
+++ Easy/SortedSquares.py
@@ -0,0 +1,23 @@
+
+#O(n) two pointer solution
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
+        
+#O(nlogn) one liner
+
+class Solution(object):
+    def sortedSquares(self, A):
+        return sorted(x*x for x in A)
